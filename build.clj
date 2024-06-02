559aacabcc5f449e029a6b8b9f1f5116a3ade6be
JRobinson28 <joseph28robinson@gmail.com>
2024-06-02 21:04:28 +0100

    Add build file

--- build.clj
+++ build.clj
@@ -1,21 +1,33 @@
 (ns build
-  (:refer-clojure :exclude [test])
-  (:require [org.corfield.build :as bb]))
+  (:require [clojure.tools.build.api :as b]))
 
-(def lib 'net.clojars.clj-template/core)
-(def version "0.1.0-SNAPSHOT")
-(def main 'clj-template.core)
+(def ^:private TARGET-DIR "target")
+(def ^:private CLASS-DIR (format "%s/classes" TARGET-DIR))
+(def ^:private UBER-FILE (format "%s/standalone.jar" TARGET-DIR))
+(def ^:private MAIN-NS 'clj-template.core)
 
-(defn test
-  "Run the tests."
-  [opts]
-  (bb/run-tests opts))
+(def ^:private basis
+  (delay (b/create-basis {:project "deps.edn"})))
 
-(defn ci
-  "Run the CI pipeline of tests (and build the uberjar)."
-  [opts]
-  (-> opts
-      (assoc :lib lib :version version :main main)
-      (bb/run-tests)
-      (bb/clean)
-      (bb/uber)))
+(defn- clean []
+  (b/delete {:path TARGET-DIR}))
+
+(defn- uber
+  [{:keys [class-dir uber-file main-ns]}]
+  (b/copy-dir {:src-dirs ["src/clj" "src/cljc" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis @basis
+                  :ns-compile [main-ns]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis @basis
+           :main main-ns}))
+
+(defn build
+  "Build an uberjar."
+  [_]
+  (clean)
+  (uber {:uber-file UBER-FILE
+         :class-dir CLASS-DIR
+         :main-ns MAIN-NS}))
